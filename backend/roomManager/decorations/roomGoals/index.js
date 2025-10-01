abc88b63e2d3cb1c771f709bab195e96eb9c1c81
Martin S <martin.stankard@hotmail.com>
2025-10-01 07:34:37 -0700

    aok oop notes

---
+++ backend/roomManager/decorations/roomGoals/index.js
@@ -0,0 +1,12 @@
+// idk, starting from task condition and standard.
+
+// i expect agents show up and hop to it, via their loop behavior.
+
+// agents have goals, maybe via their behavior, maybe via room decoration.
+
+// room goals are a list of tasks to be done, with conditions for completion. In addition to 
+
+
+// Standard is User approve || unit test pass
+
+
